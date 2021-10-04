e1350c5456037c0b6561fe72890052d7a1874e8a
MarcelloBB <marcellobelanda@gmail.com>
2021-10-04 14:38:19 -0300

    .

---
+++ LineAndCircle/LineAndCircle.c
@@ -0,0 +1,24 @@
+// PROGRAM NAME : LINEANDCIRCLE.C
+// AUTHOR       : MARCELLO
+// DATE         : 22/09/21
+
+// LIBS
+#include<stdio.h>
+//#include<conio.h>
+//#include<graphics.h>
+
+void main() {
+	//int gdriver = DETECT, gmode;
+
+	//clrscr();
+	
+	//initgraph(&gdriver, &gmode, "");
+	
+	//line(200, 200, 500, 200);
+
+	//getch();
+
+	//closegraph();
+	
+	printf("WOOOO\n");
+}
