e5c0b4109b1bbe6e7f66035632683bc10bc89501
MarcelloBB <marcellobelanda@gmail.com>
2021-10-05 09:12:09 -0300

    ADD MAIN, MAIN.c

---
+++ PrintSomeMsg/main.c
@@ -0,0 +1,12 @@
+// +-------------------------
+// PROGRAM NAME : MAIN.C
+// AUTHOR       : MARCELLO B.
+// DATE         : 05/10/21
+// +------------------------
+
+#include <stdio.h>
+
+int main(){
+	printf("\nTesting\n\n");
+	return 0;
+}
