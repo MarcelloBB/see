def4b3f9e1f5cee5f8b79f877e16d9ce58984711
MarcelloBB <marcellobelanda@gmail.com>
2021-10-19 16:36:10 -0300

    Created ./Vars/

---
+++ Basics/Vars/vars.c
@@ -0,0 +1,29 @@
+// +--------------------------------+
+//  AUTHOR       : MARCELLO BELANDA
+//  DATE         : 19/10/21
+//  PROGRAM NAME : VARS.C
+// +--------------------------------+
+
+#include <stdio.h>
+
+int main() {
+	// Declarar
+	int a,b;
+	double c;
+	char d;
+	float f;
+	
+	// Mudar
+	a = 1;
+	b = 2;
+	c = 3.2;
+	d = 'a';
+	f = 3.1;
+
+	// Mostrar
+	printf("a=%d ,b=%d\n",a,b);
+	
+
+	return 0;
+}
+
