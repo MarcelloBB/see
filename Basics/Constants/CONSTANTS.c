62fbc8b20ffb0108e90b323d0a9bb4fa188daba7
MarcelloBB <marcellobelanda@gmail.com>
2021-10-19 16:34:33 -0300

    Create ./Constants/

---
+++ Basics/Constants/CONSTANTS.c
@@ -0,0 +1,26 @@
+// +---------------------------+
+//  PROGRAM NAME : CONSTANTS.C
+//  AUTHOR       : MARCELLO B.
+//  DATE         : 19/10/21
+// +---------------------------+
+
+#include <stdio.h>
+
+//  Defining constants
+// +---------------------
+
+#define AUTHOR    "Marcello"
+#define DATE      "10/10/21"
+#define PROG_NAME "CONSTANTS.C"
+
+
+//  Main program
+// +--------------------
+
+int main(){
+	printf("Const AUTHOR    = %s\n", AUTHOR);
+	printf("Const DATE      = %s\n", DATE);
+	printf("Const PROG_NAME = %s\n", PROG_NAME);
+
+	return 0;
+}
