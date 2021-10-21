633545a12e7ded931148f781d0285000942a9445
MarcelloBB <marcellobelanda@gmail.com>
2021-10-21 12:49:01 -0300

    finish FUNCTIONS.h

---
+++ Basics/UsingHeaderFiles/functions.h
@@ -0,0 +1,32 @@
+// +--------------------------------
+//  PROGRAM NAME : FUNCTIONS.H
+//  AUTHOR       : MARCELLO BELANDA
+//  DATE         : 21/10/21
+// +--------------------------------
+//	     DESCRIPTION
+// +--------------------------------
+//  * THIS IS AN HEADER FILE (.H)
+//  * HERE YOU CAN PUT CONSTANTS,
+//    FUNCTIONS...
+// +--------------------------------
+
+// DEFINING THE NAME
+#ifndef FUNCTIONS_MODULE
+#define FUNCTIONS_MODULE
+
+// MAIN ...
+
+// CONST MY_NUMBER
+#define MY_NUMBER        10
+
+// CONST PI
+#define PI		 3.14
+
+// FUNC  SUM_ARGS
+int  SUM_ARGS(int NUM_a, int NUM_b);
+
+// FUNC  SHOW
+void SHOW(char MSG[]);
+
+// END
+#endif
