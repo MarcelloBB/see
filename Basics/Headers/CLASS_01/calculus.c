8b0db31a5d27c022f8dbc16c6245e77c0a72b6fa
MarcelloBB <marcellobelanda@gmail.com>
2021-10-23 11:52:28 -0300

    Finish CALCULUS.C

---
+++ Basics/Headers/CLASS_01/calculus.c
@@ -0,0 +1,24 @@
+// +--------------------------------
+//  PROGRAM NAME : CALCULUS.C
+//  AUTHOR       : MARCELLO BELANDA
+//  DATE         : 23/10/21 
+// +--------------------------------
+
+#include "calculus.h"
+
+float SumTwoArgs(float ArgA, float ArgB)
+{
+	return ArgA+ArgB;
+}
+
+
+int CubeArgs(int num)
+{
+	return num*num*num
+}
+
+
+int SquareArgs(int num)
+{
+	return num*num
+}
