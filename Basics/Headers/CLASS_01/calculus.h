55bd10f46397eb709c0c2a39e3de2b8cd5a1f297
MarcelloBB <marcellobelanda@gmail.com>
2021-10-23 11:46:10 -0300

    Finish CALCULUS.H

---
+++ Basics/Headers/CLASS_01/calculus.h
@@ -0,0 +1,25 @@
+// +--------------------------------
+//  PROGRAM NAME : CALCULUS.H
+//  AUTHOR       : MARCELLO BELANDA
+//  DATE         : 23/10/21 
+// +--------------------------------
+
+#ifndef CALCULUS_H_INCLUDED
+#define CALCULUS_H_INCLUDED
+
+// |CONST| _PI_
+#define _PI_       3.1415
+
+// |CONST| _FAVNUM_
+#define _FAVNUM__  27
+
+// |FUNC|  SumTwoArgs
+void SumTwoArgs (float ArgA, float ArgB);
+
+// |FUNC|  CubeArgs
+int CubeArgs(int num);
+
+// |FUNC|  SquareArgs
+int SquareArgs(int num);
+
+#endif
---
+++ Basics/Headers/CLASS_01/main.c
@@ -0,0 +1,19 @@
+// +--------------------------------
+//  PROGRAM NAME : MAIN.C
+//  AUTHOR       : MARCELLO BELANDA
+//  DATE         : 23/10/21 
+// +--------------------------------
+//	    DESCRIPTION
+// +--------------------------------
+//  * THIS FILE CONTAINS THE MAIN
+//    PROGRAM.
+// +--------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("\n\nHello, World!\n\n");
+	return 0;
+}
